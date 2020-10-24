c04ee9262b88a9569a7e1caf11c30ee6a397dc7e
Gabriel Tiso <gabrieltisovinhasdebrito2@gmail.com>
2020-10-24 12:54:20 -0300

    Feat: Basic API functionalities created. Create links, vote links and subscriptions are working

--- server/src/graphql/resolvers/Query.js
+++ server/src/graphql/resolvers/Query.js
@@ -10,10 +10,16 @@ async function feed(parent, args, context, info) {
   const links = await context.prisma.link.findMany({
     where,
     skip: args.skip,
-    take: args.take
+    take: args.take,
+    orderBy: args.orderBy
   })
 
-  return links
+  const count = await context.prisma.link.count({ where })
+
+  return {
+    links,
+    count
+  }
 }
 
 module.exports = {
